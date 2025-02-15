0ca2298cfb48ef728b3ab8baf815c87402bdf1cc
Newakk2804 <dan28.04@mail.ru>
2025-02-15 14:02:31 +0300

    file add

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".main-gallery__item img").forEach(img =>{
+    img.onclick = () => {
+        document.querySelector(".pop-up").style.display = "block";
+        document.querySelector(".pop-up img").src = img.getAttribute("src");
+    }
+});
+
+document.querySelector(".pop-up span").onclick = () => {
+    document.querySelector(".pop-up").style.display = "none";
+}
